8b433e9fc9db1053c5dd691d06abf15eb1d53ae7
Well <wellyton.jesus@insidesistemas.com.br>
2023-03-19 00:44:38 -0300

    "chore: finalizado sidebar"

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,49 +1,58 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Container } from './styles'
-// import { ReactComponent as BurgerIcon } from '@/assets/burger.svg'
-import BurgerIcon from '@/assets/burger.svg'
 import menuImg from '@/assets/menu.svg'
 
-type SidebarProps = {}
+import BurgerIcon from '@/assets/burger.svg'
+import PizzaIcon from '@/assets/pizza.svg'
+import IcecreamIcon from '@/assets/ice-cream.svg'
+import SodaIcon from '@/assets/soda.svg'
 
-export const Sidebar = (props: SidebarProps) => {
-  const [drawerOpen, setDrawerOpen] = useState(true)
+type SidebarProps = {
+}
+
+export const Sidebar = ({ }: SidebarProps) => {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [menuOption, setMenuOption] = useState<'' | 'hamburguer' | 'pizza' | 'bebidas' | 'sorvete'>('')
+
+  const handleMenuOption = (isSelected: any) => {
+    setMenuOption(isSelected);
+  }
+
+
   return (
-    <Container>
-      <button type='button'>
+    <Container className={drawerOpen ? 'active' : ''} >
+      < button onClick={() => setDrawerOpen(!drawerOpen)} type='button' >
         <img src={menuImg} alt="Menu" />
-      </button>
+      </button >
       <nav>
         <ul>
           <li>
-            <a href="#" className='active'>
-              {/* <BurgerIcon /> */}
+            <a href="#" className={menuOption === 'hamburguer' || menuOption === '' ? 'active' : ''} onClick={() => handleMenuOption('hamburguer')}>
               <img src={BurgerIcon} alt="Icone de hamburguer" />
               <span>Hamburguer</span>
             </a>
           </li>
           <li>
-            <a href="#">
-              <span></span>
+            <a href="#" className={menuOption === 'pizza' ? 'active' : ''} onClick={() => handleMenuOption('pizza')}>
+              <img src={PizzaIcon} alt="Icone de Pizza" />
+              <span>Pizza</span>
             </a>
           </li>
           <li>
-            <a href="#">
-              <span></span>
+            <a href="#" className={menuOption === 'bebidas' ? 'active' : ''} onClick={() => handleMenuOption('bebidas')}>
+              <img src={SodaIcon} alt="Icone de refrigerante" />
+              <span>Bebidas</span>
             </a>
           </li>
           <li>
-            <a href="#">
-              <span></span>
+            <a href="#" className={menuOption === 'sorvete' ? 'active' : ''} onClick={() => handleMenuOption('sorvete')}>
+              <img src={IcecreamIcon} alt="Icone de Sorvete" />
+              <span>Sorvete</span>
             </a>
           </li>
-          <li>
-            <a href="#">
-              <span></span>
-            </a>
-          </li>
+
         </ul>
       </nav>
-    </Container>
+    </Container >
   )
 }
--- src/components/sidebar/styles.ts
+++ src/components/sidebar/styles.ts
@@ -5,7 +5,7 @@ export const Container = styled.aside`
   background-color: ${theme.colors.red};
 
   width: 7.75rem;
-  /* width: 15rem; */
+  transition: 0.3s ease;
   height: 100vh;
 
   padding: 2rem 0;
@@ -16,7 +16,15 @@ export const Container = styled.aside`
   justify-content: center;
   align-items: center;
 
+  &.active {
+    width: 15rem;
+    transition: 0.3s ease;
+  }
+
   button {
+    display: inline;
+    visibility: show;
+
     background: none;
     width: 100%;
     border: none;
@@ -39,6 +47,7 @@ export const Container = styled.aside`
     li {
       list-style: none;
       a {
+        background: inherit;
         width: : fit-content;
         position: relative;
         padding-left: 1.875rem;
@@ -63,31 +72,64 @@ export const Container = styled.aside`
         }
 
         &.active {
-          &::after {
-            content: '';
-            position absolute;
-            left: 0;
-            top: 50%;
-            bottom: 0;
-            transform: translateY(-50%);
-
-            background-color: ${theme.colors.yellow};
-            width: 5px;
-            height: calc(100% + 10px);
-
-            border-radius: 0 5px 5px 0;
-          }
-
-          img path{
-            fill: ${theme.colors.yellow};
-            /* não ta funcionando */
-          }
-
-          span {
-            color: ${theme.colors.yellow}
-          }
+          border-radius: 8px;
+          background: ${theme.colors.yellow};
+          transition: 0.7s;
         }
       }
     }
   }
+
+  @media (max-width: 768px){
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+
+    width: 100%;
+    height: 5rem;
+    padding: 0 0;
+    overflow-y: auto;
+
+    button {
+    display: none;
+    visibility: hidden;
+  }
+
+  nav {
+    height: 100%;
+
+    ul {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      li {
+
+        a{
+          flex-direction: column;
+          padding: 0rem;
+
+          img {
+            width: 3.25rem;
+            height: 3.25rem;
+          }
+        }
+
+        span {
+        display: none;
+      }
+      }
+
+      
+    }
+  }
+  
+
+  }
+
+
+  
 `
